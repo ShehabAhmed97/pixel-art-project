8c87a663ac230aaf00d156727d573ef8d9d4aa63
Shehab Ahmed Khalil <shehabkhaleel@gmail.com>
2021-01-20 23:39:20 +0200

    First update

--- designs.js
+++ designs.js
@@ -1,10 +1,17 @@
 // Select color input
 // Select size input
-
+let colorSelected = document.getElementById("colorPicker").value
+var gridHeight = document.getElementById("inputHeight").value
+var gridWidth = document.getElementById("inputWidth").value
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+    for(i=1;i<=gridHeight;i++){
+        document.getElementById('pixelCanvas').insertAdjacentHTML('beforeend','<tr id="tableRow"><td>i</td></tr>')
+    }
 
 // Your code goes here!
 
 }
+
+document.getElementById('submit').addEventListener('click',function(event){makeGrid();event.preventDefault})
