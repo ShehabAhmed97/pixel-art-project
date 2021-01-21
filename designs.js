e914ee1df7df558fd34ad9a6f96325a9aca41129
Shehab Ahmed Khalil <shehabkhaleel@gmail.com>
2021-01-21 15:06:56 +0200

    final app color picker, grid sizing and automatically drawn, changing colors on click

--- designs.js
+++ designs.js
@@ -1,17 +1,45 @@
 // Select color input
-// Select size input
-let colorSelected = document.getElementById("colorPicker").value
-var gridHeight = document.getElementById("inputHeight").value
-var gridWidth = document.getElementById("inputWidth").value
-// When size is submitted by the user, call makeGrid()
+let colorSelector=document.getElementById("colorPicker");
+let colorSelected=document.getElementById("colorPicker").value;
+colorSelector.addEventListener('input',function(e){
+    colorSelected=e.target.value;
+    e.preventDefault;
+},true)
 
-function makeGrid() {
-    for(i=1;i<=gridHeight;i++){
-        document.getElementById('pixelCanvas').insertAdjacentHTML('beforeend','<tr id="tableRow"><td>i</td></tr>')
+// Select size input
+document.getElementById('sizePicker').addEventListener('submit',makeGrid)
+
+//function runs when the user submits the size of grid
+function makeGrid(event) {
+
+    //get number of grid rows
+    let gridHeight = document.getElementById("inputHeight").value
+
+    //get number of grid rows
+    let gridWidth = document.getElementById("inputWidth").value
+
+    //adding cells of the the grid according to size provided from user
+    for(i=0;i<=gridHeight-1;i++){
+        document.getElementById('pixelCanvas').insertAdjacentHTML('beforeend','<tr class="tableRow"></tr>')
+        for(j=0;j<=gridWidth-1;j++){
+            document.querySelectorAll('.tableRow')[i].insertAdjacentHTML('beforeend','<td class="cell"></td>')
+        }
     }
 
+    //get total number of cells 
+    let cells=document.querySelectorAll('.cell')
+
+    //changing color of cells on click of user, to the color that user picked
+    for(x=0;x<=cells.length-1;x++){
+        cells[x].addEventListener('click',function(ev){
+            ev.target.setAttribute('style',`background-color:${colorSelected}`)
+            ev.preventDefault();
+        })
+    }
+    event.preventDefault()
+}
 // Your code goes here!
 
-}
 
-document.getElementById('submit').addEventListener('click',function(event){makeGrid();event.preventDefault})
+
+
